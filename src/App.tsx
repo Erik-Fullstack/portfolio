8cde5d944d72192f9f06cfd0959affad53e0142c
Erik-Fullstack <erik.andersson@chasacademy.se>
2025-07-23 16:42:18 +0200

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import About from './pages/About'
 import SecondTest from './pages/Projects'
 import Header from './components/Header'
 
-// TODO: fixa size på profilkort för mobile view, fixa responsive mobile+padda view
-// fixa styling generellt, fixa fonts, kolla på text ser bra ut i mobile view
+// TODO:
+// lägg till mina stacks, cv
 //footer?
 
 function App() {
 
   return (
     <div className="min-h-screen">
-      <Header/>
+      <Header />
       <Hero />
       <About />
       <SecondTest />
