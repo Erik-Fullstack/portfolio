f7fc974312eaec134b8c2b8d04453a31f9d90354
Erik-Fullstack <erik.andersson@chasacademy.se>
2025-07-23 16:40:03 +0200

    more responsiveness

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,12 +5,12 @@ import reactIcon from "../assets/react.svg"
 export default function TestPage() {
 
     return (
-        <div className="min-h-screen relative z-10 bg-gray-900 border-t-1 border-b-1 border-[#293c64] flex justify-center items-center" id='About'>
+        <div className="pt-4 pb-4 sm:pt-0 sm:pb-0 min-h-screen relative z-10 bg-gray-900 border-t-1 border-b-1 border-[#293c64] flex justify-center items-center" id='About'>
             <main className="w-full max-w-screen-2xl px-20">
-                <section className="flex flex-col-reverse p-10 w-full justify-between items-center gap-5 relative md:flex-row md:gap-20">
+                <section className="flex flex-col-reverse md:p-10 w-full justify-between items-center gap-5 relative md:flex-row md:gap-20">
                     <div className='md:max-w-140 mx-auto'>
-                        <h3 className='text-center text-3xl md:text-left lg:text-5xl '>Erik Andersson</h3>
-                        <p className='text-lg md:pt-4 md:text-1xl lg:text-3xl'>I am a student based in Stockholm, currently enrolled in the Fullstack Javascript program at Chas Academy. </p>
+                        <h3 className='text-center text-2xl md:text-left lg:text-5xl '>Erik Andersson</h3>
+                        <p className='text-lg pt-2 md:pt-4 md:text-1xl lg:text-3xl'>I am a student based in Stockholm, currently enrolled in the Fullstack Javascript program at Chas Academy. </p>
                         <p className='text-lg md:pt-2 md:text-1xl lg:text-3xl'>I am currently looking for an internship for the period December 2025 until May 2026.</p>
                         <p className='text-lg md:pt-2 md:text-1xl lg:text-3xl'>Please feel free to reach out if this sounds interesting.</p>
                         <div className='flex gap-4 justify-evenly pt-2 lg:pt-15'>
--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -7,10 +7,10 @@ export default function Hero() {
         <div className='min-h-screen z-3 sticky top-0'>
             <Particles />
             <Hyperspeed />
-            {/* <div className="flex flex-col relative z-5 items-center mx-auto pt-60">
+            <div className="flex flex-col relative z-5 items-center mx-auto pt-60">
                 <DecryptedText
-                    encryptedClassName="text-7xl font-semibold"
-                    className="text-7xl font-semibold"
+                    encryptedClassName="text-2xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold"
+                    className="text-2xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold"
                     text="<ERIK ANDERSSON/>"
                     animateOn="view"
                     sequential={true}
@@ -19,16 +19,16 @@ export default function Hero() {
                     useOriginalCharsOnly={false}
                 />
                 <DecryptedText
-                    encryptedClassName="text-[3.4rem] font-semibold"
-                    className="text-[3.4rem] font-semibold"
-                    text="<Fullstack JS/>"
+                    encryptedClassName="text-2xl lg:text-[3.4rem] font-semibold"
+                    className="text-[1.4rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[3.4rem] font-semibold"
+                    text="<FULLSTACK JS/>"
                     animateOn="view"
                     sequential={true}
                     revealDirection="start"
                     speed={60}
                     useOriginalCharsOnly={false}
                 />
-            </div> */}
+            </div>
         </div>
     )
 }
